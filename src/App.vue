9981fa73e98b1746868a8416b89cfa1e7eb10fe4
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2020-12-09 17:57:07 +0100

    request weather

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,23 @@ export default {
   name: 'App',
   components: {
   },
+  mounted() {
+    this.getWeather();
+  },
+  methods: {
+    async getWeather() {
+      try {
+        const host = process.env.VUE_APP_HOST;
+        const key = process.env.VUE_APP_KEY;
+        console.log(host, key);
+        const response = await fetch(`${host}=London&appid=${key}`);
+        const result = await response.json();
+        console.log(result);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 };
 </script>
 
